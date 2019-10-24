6cc55fd8201510e01f57552f1ec4190af8216551
kissanmani <iamdeveloper1993@gmail.com>
2019-10-24 18:08:39 +0530

    1 file modified

--- App.js
+++ App.js
@@ -1,78 +1,106 @@
-import React, { Component } from 'react';
-import Users from './components/Users';
-import AddUser from './components/AddUser';
+import React, { Component } from "react";
+import Users from "./components/Users";
+import AddUser from "./components/AddUser";
 
-class App extends Component{
-
-    // Default dummy data
-    state = {
-
-        users:
-        [
-          {avatarImg: "https://img.mobiscroll.com/demos/Requiem_for_a_Dream.png", name:"Kissan", companyName:"gtpl", emailId:"mani@gmail.com", contactNo:"8344887993", designation:"developer", isEditing:false}
-        ]
+class App extends Component {
+  // Default dummy data
+  state = {
+    users: [
+      {
+        avatarImg: "https://img.mobiscroll.com/demos/Requiem_for_a_Dream.png",
+        name: "Kissan",
+        companyName: "gtpl",
+        emailId: "mani@gmail.com",
+        contactNo: "8344887993",
+        designation: "developer",
+        isEditing: false
       }
-      addUser = (newUser) => {
-            let users = [...this.state.users, newUser];
-            this.setState({
-                users
-            });     
-      }
-      
-      pressViewBtn = (i) => {
-        alert ();
-      }
+    ]
+  };
+  addUser = newUser => {
+    let that = this;
+    fetch("https://randomuser.me/api/?gender=male")
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
 
-
-      pressEditBtn = (i) => {
-        let users = this.state.users;
-        users[i].isEditing = true;
-        this.setState({
+        // Examine the text in the response
+        response.json().then(function(data) {      
+          const derivedState = {
+            ...newUser,
+            avatarImg: data.results[0].picture.thumbnail
+          }
+          let users = [...that.state.users, derivedState];
+          that.setState({
             users
+          })
         });
-      }
+      })
+      .catch(function(err) {
+        console.log("Fetch Error :-S", err);
+      });
+    
+  };
 
-      updateUser = (i, name, companyName, emailId, contactNo, designation) => {
-        let users = this.state.users;
-        users[i].avatarImg = avatarImg;
-        users[i].name = name;
-        users[i].companyName = companyName;
-        users[i].emailId = emailId;
-        users[i].contactNo = contactNo;
-        users[i].designation = designation;
-        users[i].isEditing = false;
+  pressViewBtn = i => {
+    alert();
+  };
 
-        this.setState({
-            users
-        });
+  pressEditBtn = i => {
+    let users = this.state.users;
+    users[i].isEditing = true;
+    this.setState({
+      users
+    });
+  };
 
-      }
+  updateUser = (i, name, companyName, emailId, contactNo, designation) => {
+    let users = this.state.users;
+    users[i].name = name;
+    users[i].companyName = companyName;
+    users[i].emailId = emailId;
+    users[i].contactNo = contactNo;
+    users[i].designation = designation;
+    users[i].isEditing = false;
 
+    this.setState({
+      users
+    });
+  };
 
-      pressDelete = (i) => {
-        let users = this.state.users.filter((u,index)=>{
-            return index !== i;
-        });
-        this.setState({
-            users
-        });
-      }
+  pressDelete = i => {
+    let users = this.state.users.filter((u, index) => {
+      return index !== i;
+    });
+    this.setState({
+      users
+    });
+  };
 
-    render(){
-        return(
-            <div className="Main-Wrapper">
-              <center>
-                <h1 className="titleText">Employee Management App</h1>
-              </center>
-              <div className="block-addEmployee">
-                <AddUser addUser={this.addUser}/> 
-              </div>
-                
-                <Users allUsers={this.state.users}pressViewBtn={this.pressViewBtn} pressEditBtn={this.pressEditBtn} updateUser={this.updateUser} pressDelete={this.pressDelete} />
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Main-Wrapper">
+        <center>
+          <h1 className="titleText">Employee Management App</h1>
+        </center>
+        <div className="block-addEmployee">
+          <AddUser addUser={this.addUser} />
+        </div>
+
+        <Users
+          allUsers={this.state.users}
+          pressViewBtn={this.pressViewBtn}
+          pressEditBtn={this.pressEditBtn}
+          updateUser={this.updateUser}
+          pressDelete={this.pressDelete}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
